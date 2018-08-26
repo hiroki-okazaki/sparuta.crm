6cf86e8544090416b1805efb3ce3ec91ec258417
hiroki-okazaki <1031okazaki@gmail.com>
2018-08-26 18:46:28 +0900

    完成

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,15 @@
 class Customer < ApplicationRecord
+  belongs_to :company
+  belongs_to :post
+
+############ ここから追加
+  validates :family_name, presence: true, length: { maximum: 20 }
+  validates :given_name, presence: true, length: { maximum: 20 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
+  ############ ここまで追加
+
+  def full_name
+    family_name + given_name
+  end
 end
